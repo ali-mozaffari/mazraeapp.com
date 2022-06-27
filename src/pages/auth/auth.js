ca8e10eeee82b6e0393592a87fb28f4f45d91f53
ali <a.mozaffari10@gmail.com>
2022-06-27 15:02:36 +0430

    login3

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -25,8 +25,8 @@ function Copyright(props) {
       {...props}
     >
       {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="https://mazraeapp.com//">
+        mazraeapp
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
