42f9f123a8b022df5cd74af19510e96658a5d672
amir mahdi  asadi <amir.asadi13@yahoo.com>
2022-07-12 22:38:49 -1200

    initial created add activity page

--- src/services/config.js
+++ src/services/config.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 // export const apiUrl = 'https://ea65-185-16-206-238.eu.ngrok.io'
-axios.defaults.baseURL = "https://2039-104-237-224-206.ngrok.io"
+axios.defaults.baseURL = "https://57be-151-80-6-46.eu.ngrok.io"
 
